ddb44ec6607fb25cd4e84ac0a55e5977994f144e
buvankarthik <karthikbuvan.bu@gmail.com>
2026-09-06 11:41:14 +0200

    Entities project

---
+++ Server/Entities/Comment.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities;
+
+public class Comment
+{
+    public int commentId { get; set; }
+    public string body { get; set; }
+    public int userId { get; set; }
+    public int postId { get; set; }
+}
---
+++ Server/Entities/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Entities;
+
+public class User
+{
+    private int userId { get; set; }
+    private string name { get; set; }
+    private string password { get; set; }
+}
